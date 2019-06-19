9ffdd8820559e5ef96dd461d5a473bb04173070b
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-06-19 13:57:15 +0530

    added a separate errors file added exception for missing parameter

--- src/util/errors.kt
+++ src/util/errors.kt
@@ -2,3 +2,5 @@ package com.nooblabs.util
 
 class AuthenticationException : RuntimeException()
 class AuthorizationException : RuntimeException()
+
+class MissingParameter(val param: String) : RuntimeException()
