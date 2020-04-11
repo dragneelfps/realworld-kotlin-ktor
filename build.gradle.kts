c7f87ba466f0718460e59f72df997d3c2a3e69c2
Sourabh Singh Rawat <s.rawat3.142@live.in>
2020-04-11 23:52:36 +0530

    Switch to .kts build files.

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version kotlinVersion
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+}
+
+group = "realworld"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("testresources")
+
+repositories {
+    mavenLocal()
+    jcenter()
+}
+
+dependencies {
+    implementation(Deps.kotlinLib)
+    implementation(Deps.ktorServerNetty)
+    implementation(Deps.logback)
+    implementation(Deps.ktorAuthJwt)
+    implementation(Deps.ktorJackson)
+
+    implementation(Deps.h2Database)
+    implementation(Deps.exposed)
+    implementation(Deps.hikari)
+
+    testImplementation(Deps.ktorTests)
+}
